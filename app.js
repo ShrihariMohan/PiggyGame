48a6ad67924c848618ba4367cf08ceeca5a4d80a
Shrihari Mohan <shrihari.uma.1999@gmail.com>
2020-08-08 19:40:29 +0530

    Added Game Details Via Alerts

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ function addFunction()
 
 var scores , roundScore , activePlayer , gamePLay  ;
 function init (){
+    alert(" Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to 'hold': If the player rolls a 1, they score nothing and it becomes the next player's turn. If the player rolls any other number, it is added to their turn total and the player's turn continues.");
 gamePLay = true ;
 scores = [ 0 ,0] ;
 roundScore = 0 ;
